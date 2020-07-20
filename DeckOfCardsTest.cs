a1eeef065210dcc1ee63e37ce9aa9019d77dddc4
gvega001 <gvega001@gmail.com>
2020-07-20 12:24:58 -0700

    change name of file

---
+++ DeckOfCardsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace online.dev4you
+{
+    class DeckOfCardsTest
+    {
+        static void Main()
+        {
+            var myDeckOfCards = new DeckOfCards();
+            myDeckOfCards.shuffle(); // Place the cards in random order
+
+            //display all the cards. Should deal 52 totals
+            for (int i = 0; i < 50; ++i)
+            {
+                Console.WriteLine($"{myDeckOfCards.DealCard(),-19}");
+                if ((i + 1)% 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
